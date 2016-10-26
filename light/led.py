fb3cd8f4d5dc808170175875fe2c804d07627c43
Somchai Somphadung <somchaisomph@gmail.com>
2016-10-26 19:41:21 +0700

    Update led.py

--- light/led.py
+++ light/led.py
@@ -12,4 +12,18 @@ class LED(threading.Thread):
           except :
                raise
                
-    
+     def run(self):
+          self.stop_flag = False
+          while not self.stop_flag :
+               pass
+          
+     def blink(self,interval=0.1,times=2):
+          for i in range(times):
+               self._led.write(1)
+               time.sleep(interval)
+               self._led.write(0)
+               time.sleep(interval)
+               
+     def end(self):
+          self.stop_flag = True
+          self.join()
