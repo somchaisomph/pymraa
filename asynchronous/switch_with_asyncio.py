6ad6bf7af7c686ec7b7aa909a7521c036f08df4e
Somchai Somphadung <somchaisomph@gmail.com>
2016-10-27 12:43:08 +0700

    Update switch_with_asyncio.py

--- asynchronous/switch_with_asyncio.py
+++ asynchronous/switch_with_asyncio.py
@@ -1,2 +1,25 @@
 import mraa
 import asyncio
+#Tested with only python 3.4 installed on Raspbian
+@asyncio.coroutine
+def operate():
+  global sw, led
+  state = sw.read()
+  led.write(state)
+  yield from asyncio.sleep(0.1)
+  asyncio.async(operate())
+  
+if __name__ == "__main__":
+  sw = mraa.Gpio(12) # attach pin 12 to one pin of switch
+  led = mraa.Gpio(40) # attach pin 40 to LED
+  sw.dir(mraa.DIR_IN)
+  led.dir(mraa.DIR_OUT)
+  loop = asynio.get_event_loop()
+  try:
+    asyncio.async(operate())
+    loop.run_for_ever()
+  except KeyboardInterrupt :
+    pass
+  finally:
+    loop.close()
+
